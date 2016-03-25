1261eedbd93de1288bdb9e90fe7da668c4673a72
m4b <m4b.github.io@gmail.com>
2016-03-24 23:32:05 -0700

    build, tests: put -l flags after everything because machines are jerks

--- gen_tests.sh
+++ gen_tests.sh
@@ -9,20 +9,20 @@ TESTDIR=test
 echo -e "PT_INTERPRETER for $TESTDIR/ binaries is $DRYAD\nBinaries prefixed with 'ld' use the system dynamic linker, ld-linux-x86-64.so.2"
 
 echo -e "Building regular binary $TESTDIR/test with libm and libc"
-gcc -lm -Wl,-I,$DRYAD $TESTDIR/test.c -o $TESTDIR/test
-gcc -lm $TESTDIR/test.c -o $TESTDIR/ldtest
+gcc -Wl,-I,$DRYAD $TESTDIR/test.c -o $TESTDIR/test -lm
+gcc $TESTDIR/test.c -o $TESTDIR/ldtest -lm
 
 echo -e "Building thread local binary $TESTDIR/tlocal with pthreads and libc"
-gcc -lpthread -Wl,-I,$DRYAD $TESTDIR/tlocal.c -o $TESTDIR/tlocal
-gcc -lpthread $TESTDIR/tlocal.c -o $TESTDIR/ldtlocal
+gcc -Wl,-I,$DRYAD $TESTDIR/tlocal.c -o $TESTDIR/tlocal -lpthread
+gcc $TESTDIR/tlocal.c -o $TESTDIR/ldtlocal -lpthread
 
 echo -e "Building complicated binary $TESTDIR/snappy linked with libm and snappy (on my system snappy uses libstdc++.so.6, libm.so.6, libc.so.6, and libgcc_s.so.1)"
-gcc -lm -lsnappy -Wl,-I,$DRYAD $TESTDIR/snappy.c -o $TESTDIR/snappy
-gcc -lm -lsnappy $TESTDIR/snappy.c -o $TESTDIR/ldsnappy
+gcc -Wl,-I,$DRYAD $TESTDIR/snappy.c -o $TESTDIR/snappy -lm -lsnappy
+gcc $TESTDIR/snappy.c -o $TESTDIR/ldsnappy -lm -lsnappy
 
 echo -e "Building a binary $TESTDIR/float with libm that performs no printing"
-gcc -lm -Wl,-I,$DRYAD $TESTDIR/float.c -o $TESTDIR/float
-gcc -lm $TESTDIR/float.c -o $TESTDIR/ldfloat
+gcc -Wl,-I,$DRYAD $TESTDIR/float.c -o $TESTDIR/float -lm
+gcc $TESTDIR/float.c -o $TESTDIR/ldfloat -lm
 
 # uncomment this and use $LIB with a `libc.so` to create a musl binary to test with
 #echo -e "Building musl linked binary $TESTDIR/musl"
