de279b3431234746bcfa75fe7bf9a147be3a3058
m4b <m4b.github.io@gmail.com>
2016-04-13 23:16:37 -0700

    build: add source to travis script

--- setup.sh
+++ setup.sh
@@ -3,5 +3,6 @@
 curl -O https://static.rust-lang.org/rustup/dist/x86_64-unknown-linux-gnu/rustup-setup
 chmod +x rustup-setup
 ./rustup-setup -y --default-toolchain nightly
+source ~/.cargo/env
 rustup default nightly
 rustup target add x86_64-unknown-linux-musl
