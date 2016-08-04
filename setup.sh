77a9da3852f84892b8fb830d60fd78b5aae3b4bc
m4b <m4b.github.io@gmail.com>
2016-08-03 22:02:11 -0700

    build: update rustup url and print some versions for debugging

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -e
 
 cd /tmp
-curl -O https://static.rust-lang.org/rustup/dist/x86_64-unknown-linux-gnu/rustup-setup
+curl https://sh.rustup.rs > rustup-setup
 chmod +x rustup-setup
 ./rustup-setup -y --default-toolchain nightly
 source ~/.cargo/env
 rustup default nightly
 rustup target add x86_64-unknown-linux-musl
+rustup show
+ld -v
